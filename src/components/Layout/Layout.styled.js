7b4717bc7e48e92d838cd31e9d3cae5d335947d4
Denis <grabko.denis11@gmail.com>
2023-12-29 19:36:38 +0200

    styled-comp-add

--- src/components/Layout/Layout.styled.js
+++ src/components/Layout/Layout.styled.js
@@ -1,7 +1,9 @@
+import styled from 'styled-components';
+
 export const Section = styled.section`
-    display: flex;
-    max-width: 1400px;
-    margin-right: auto;
-    margin-left: auto;
-    gap: 10px;
+  display: flex;
+  max-width: 1400px;
+  margin-right: auto;
+  margin-left: auto;
+  gap: 10px;
 `;
